a57ea4b4515fb0a0c35d260d1a1ad55dd6f5882f
Rob <robertwojcik1993@gmail.com>
2017-11-07 18:52:45 +0100

    affter pull

--- src/test/java/pl/sdacademy/animals/bear/BearTest.java
+++ src/test/java/pl/sdacademy/animals/bear/BearTest.java
@@ -1,6 +1,9 @@
 package pl.sdacademy.animals.bear;
 
+import org.joda.time.DateTime;
+import org.joda.time.Duration;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class BearTest {
@@ -13,5 +16,40 @@ class BearTest {
         boolean result = bear.isAlive();
 
         assertTrue(result == true);
+    }
+
+    @Test
+    void feedingBearShouldSetFeedingTimeForNow() {
+        Bear bear = new BlackBear(1);
+        bear.eat();
+
+        assertTrue(new Duration(DateTime.now(), bear.getLastMealTime()).isShorterThan(Duration.standardSeconds(1)));
+
+    }
+
+    @Test
+    void bearShouldBeDeadAfter10DaysSinceLastMeal(){
+        Bear bear = new BlackBear(1,new TestClock());
+
+        assertTrue(bear.isAlive() == false);
+
+    }
+
+    @Test
+    void bearShouldIncreaseMassAfterMeal(){
+        Bear bear = new BlackBear(1);
+        int oldWeight = bear.getWeight();
+        bear.eat(4);
+
+        assertTrue(bear.getWeight() > oldWeight);
+    }
+
+    @Test
+    void bearShouldIncreaseMassAfterDrink(){
+        Bear bear = new BlackBear(1);
+        int oldWeight = bear.getWeight();
+        
+        bear.drink(10);
+        assertTrue(bear.getWeight() > oldWeight);
     }
 }
